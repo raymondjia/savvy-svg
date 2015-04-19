ba26774337ffe73e95488d051150cdc467d1290a
Raymond Jia <raymond@gmail.com>
2015-04-19 08:09:04 -0700

    Adding axillary files

---
+++ README.md
@@ -0,0 +1,8 @@
+# SVG to PNG Conversion Tool Optimized for Mobile Development
+
+This is still very much work in progress. Expect lots of rough edges.
+
+## Installation
+
+It depends on a minor fix I made to domenic's svg2png. You should use my forked version instead.
+
--- config_local.js
+++ config_local.js
@@ -1,13 +1,22 @@
+// You can use either absolute or relative path in 'sourceDir' and 'targetDir'.
+// Do not use '~'
+
 module.exports = {
     projects: [
         { 
-            name: 'iOS demo',
+            name: 'moblabios-judge-me-not',
+            sourceDir: '/Users/raymond/Workspace/moblab-ios/Moblab_iOS/Res/Game/AllGame/JudgeMeNot/_kitchen',
+            targetDir: '/Users/raymond/Workspace/moblab-ios/Moblab_iOS/Res/Game/AllGame/JudgeMeNot',
+            platform: 'iOS',
+        },
+        { 
+            name: 'ios-demo',
             sourceDir: './ios_demo/_kitchen',
             targetDir: './ios_demo/',
             platform: 'iOS',
         },
         { 
-            name: 'Android demo',
+            name: 'android-demo',
             sourceDir: './android_demo/_svg_source',
             targetDir: './android_demo/',
             platform: 'Android',

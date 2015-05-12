315387e818dfd69d0b86650d4738c5ac2dda349e
Raymond Jia <raymondjia@gmail.com>
2015-05-12 18:40:42 +0800

    even more unit tests

--- test/tests.js
+++ test/tests.js
@@ -6,6 +6,7 @@ var should = require("chai").should();      // jshint ignore:line
 var fs = require("fs");
 var tempfs = require("temp-fs");
 var _ = require("lodash");
+var path = require("path");
 
 var rewire = require("rewire");
 var savvysvg = rewire("..");
@@ -156,21 +157,26 @@ describe("Parsing META file", function() {
 
         var metaFilePath;
 
-        before(function() {
+        before(function(done) {
             tempfs.track();
             tempfs.open(function(err, tempFile) {
                 should.not.exist(err);
                 fs.writeSync(tempFile.fd, testData);
                 fs.closeSync(tempFile.fd);
                 metaFilePath = tempFile.path;
-
+                done();
             });
         });
 
         it("read every line", function() {
+            var mockConsole = {
+                error: function() {}
+            };
+            var revert = savvysvg.__set__("console", mockConsole);
             var meta = savvysvg.parseMetadataFile(metaFilePath);
+            revert();
             meta.should.have.property("line4");
-            meta.should.have.property("line6");
+            meta.should.have.property("line6AltName");
         });
 
         it("should keep track of line number correctly", function() {
@@ -194,127 +200,221 @@ describe("Parsing META file", function() {
 
 
 describe("Handling of project", function() {
+
+    var testDataArray = [
+        "file1 1.0x",
+        "file1 0.8x file1AltName",
+        "file2 200 100",
+        "file2 210 110 file2AltName"
+    ];
+    var testData = testDataArray.join("\n");
+
+    var projectPath;
+
+    before(function(done) {
+        tempfs.track();
+        tempfs.mkdir(function(err, dir) {
+            projectPath = dir.path;
+            done();
+        });
+    });
+
     it("should report error if META file is not found", function() {
+        var testProject =  { 
+            name: "iostest",
+            sourceDir: projectPath,
+            targetDir: projectPath,
+            platform: "iOS",
+        };
 
+        var errorMessage;
+        var mockConsole = {
+            error: function(arg) {
+                errorMessage = arg;
+            }
+        };
+        var revert = savvysvg.__set__("console", mockConsole);
+        savvysvg.handleProject(testProject);
+        revert();   
+        errorMessage.should.match(/Failed to open META file/);
     });
 
-    describe("Reading META file", function() {
+    it("should enqueue files for processing - iOS", function() {
+        fs.writeFileSync(path.join(projectPath, "META.txt"), testData);
+        var testProject =  { 
+            name: "iostest",
+            sourceDir: projectPath,
+            targetDir: projectPath,
+            platform: "iOS",
+        };
 
+        savvysvg.handleProject(testProject);
+        var queue = savvysvg.globalQueue.queue;
+
+        queue.length.should.equal(testDataArray.length * 3);        // each SVG file should generate 3 PNG
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1.png"),
+            scale: 1.0,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1@2x.png"),
+            scale: 2.0,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1@3x.png"),
+            scale: 3.0,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1AltName.png"),
+            scale: 0.8,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1AltName@2x.png"),
+            scale: 0.8 * 2,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file1.svg"),
+            targetPath: path.join(projectPath, "file1AltName@3x.png"),
+            scale: 0.8 * 3,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2.png"),
+            width: 200,
+            height: 100,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2@2x.png"),
+            width: 400,
+            height: 200,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2@3x.png"),
+            width: 600,
+            height: 300,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2AltName.png"),
+            width: 210,
+            height: 110,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2AltName@2x.png"),
+            width: 420,
+            height: 220,
+            mustRun: true
+        });
+        queue.should.include({
+            sourcePath: path.join(projectPath, "file2.svg"),
+            targetPath: path.join(projectPath, "file2AltName@3x.png"),
+            width: 630,
+            height: 330,
+            mustRun: true
+        });
     });
 
-    describe("Detecting changes in META file", function() {
-        
+    it("save a shadow copy of the META file", function() {
+        var cachedData = fs.readFileSync(path.join(projectPath, ".cachedMETA.txt"), { encoding: "ascii" });
+        cachedData.should.equal(testData);
+    });
+
+    it("should enqueue a file as optional if definition is not changed", function() {
+       var testProject =  { 
+            name: "iostest",
+            sourceDir: projectPath,
+            targetDir: projectPath,
+            platform: "iOS",
+        };
+
+        savvysvg.globalQueue.flush();
+        savvysvg.handleProject(testProject);
+        var queue = savvysvg.globalQueue.queue;
+
+        queue.length.should.equal(testDataArray.length * 3);        // each SVG file should generate 3 PNG
+ 
+        _.each(queue, function(item) {
+            item.mustRun.should.equal(false);
+        });
+    });
+
+    it("should detect changes in META file", function() {        
+        var cachedDataArray = [
+            "file1 1.0x",
+            "file1 0.8x file1AltName",
+            "file2 210 110 file2AltName"
+        ];
+        var cachedData = cachedDataArray.join("\n");
+        fs.writeFileSync(path.join(projectPath, ".cachedMETA.txt"), cachedData);
+
+        var testProject =  { 
+            name: "iostest",
+            sourceDir: projectPath,
+            targetDir: projectPath,
+            platform: "iOS",
+        };
+
+        savvysvg.globalQueue.flush();
+        savvysvg.handleProject(testProject);
+        var queue = savvysvg.globalQueue.queue;
+
+        queue.length.should.equal(testDataArray.length * 3);        // each SVG file should generate 3 PNG
+
+        _.each(queue, function(item) {
+            if (item.targetPath === path.join(projectPath, "file2AltName@3x.png")) {
+                item.mustRun.should.equal(false);
+            }
+            else if (item.targetPath === path.join(projectPath, "file2@2x.png")) {
+                item.mustRun.should.equal(true);
+            }
+        });
+    });    
+
+    it("should enqueue all files as must run if -f is specified", function() {
+       var testProject =  { 
+            name: "iostest",
+            sourceDir: projectPath,
+            targetDir: projectPath,
+            platform: "iOS",
+        };
+
+        var mockConfig = { force: true };
+        var revert = savvysvg.__set__("config", mockConfig);
+
+        savvysvg.globalQueue.flush();
+        savvysvg.handleProject(testProject);
+        revert();
+
+        var queue = savvysvg.globalQueue.queue;
+
+        queue.length.should.equal(testDataArray.length * 3);        // each SVG file should generate 3 PNG
+ 
+        _.each(queue, function(item) {
+            item.mustRun.should.equal(true);
+        });
+    });
+
+    after(function() {
+        fs.unlinkSync(path.join(projectPath, "META.txt"));
+        fs.unlinkSync(path.join(projectPath, ".cachedMETA.txt"));
     });
 });
 
-
-specify("Command line arguments", function(done) {
-    done();
-});
-
-/*
-specify("Scale 1.svg to 80%", function (done) {
-    svg2png(relative("images/1.svg"), relative("images/1-actual.png"), 0.8, function (err) {
-        if (err) {
-            return done(err);
-        }
-
-        var expected = fs.readFileSync(relative("images/1-expected.png"));
-        var actual = fs.readFileSync(relative("images/1-actual.png"));
-
-        actual.should.deep.equal(expected);
-
-        done();
-    });
-});
-
-specify("Scale 2.svg to 180%", function (done) {
-    svg2png(relative("images/2.svg"), relative("images/2-actual.png"), 1.8, function (err) {
-        if (err) {
-            return done(err);
-        }
-
-        var expected = fs.readFileSync(relative("images/2-expected.png"));
-        var actual = fs.readFileSync(relative("images/2-actual.png"));
-
-        actual.should.deep.equal(expected);
-
-        done();
-    });
-});
-
-specify("Omit scale argument for 3.svg", function (done) {
-    svg2png(relative("images/3.svg"), relative("images/3-actual.png"), function (err) {
-        if (err) {
-            return done(err);
-        }
-
-        var expected = fs.readFileSync(relative("images/3-expected.png"));
-        var actual = fs.readFileSync(relative("images/3-actual.png"));
-
-        actual.should.deep.equal(expected);
-
-        done();
-    });
-});
-
-specify("No green border for 4.svg", function (done) {
-    svg2png(relative("images/4.svg"), relative("images/4-actual.png"), function (err) {
-        if (err) {
-            return done(err);
-        }
-
-        var expected = fs.readFileSync(relative("images/4-expected.png"));
-        var actual = fs.readFileSync(relative("images/4-actual.png"));
-
-        actual.should.deep.equal(expected);
-
-        done();
-    });
-});
-
-specify("Scales 5.svg correctly despite viewBox + fixed width/height", function (done) {
-    svg2png(relative("images/5.svg"), relative("images/5-actual.png"), 2, function (err) {
-        if (err) {
-            return done(err);
-        }
-
-        var expected = fs.readFileSync(relative("images/5-expected.png"));
-        var actual = fs.readFileSync(relative("images/5-actual.png"));
-
-        actual.should.deep.equal(expected);
-
-        done();
-    });
-});
-
-it("should pass through errors that occur while calculating dimensions", function (done) {
-    svg2png(relative("images/invalid.svg"), relative("images/invalid-actual.png"), function (err) {
-        should.exist(err);
-        err.should.have.property("message").and.match(/Unable to calculate dimensions./);
-
-        done();
-    });
-});
-
-it("should pass through errors about unloadable source files", function (done) {
-    svg2png("doesnotexist.asdf", "doesnotexist.asdf2", 1.0, function (err) {
-        should.exist(err);
-        err.should.have.property("message").that.equals("Unable to load the source file.");
-
-        done();
-    });
-});
-
-after(function () {
-    fs.unlink(relative("images/1-actual.png"));
-    fs.unlink(relative("images/2-actual.png"));
-    fs.unlink(relative("images/3-actual.png"));
-    fs.unlink(relative("images/4-actual.png"));
-    fs.unlink(relative("images/5-actual.png"));
-});
-
-function relative(relPath) {
-    return path.resolve(__dirname, relPath);
-}
-*/
